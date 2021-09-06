ef7870bdc9c964fe7f73cc2a595b8d38f351feb0
imasmitja <58853765+imasmitja@users.noreply.github.com>
2021-09-05 21:49:37 -0700

    test 48 update

--- td3_bc.py
+++ td3_bc.py
@@ -42,10 +42,10 @@ class TD3_BCAgent():
         hard_update(self.target_actor, self.actor)
         hard_update(self.target_critic, self.critic)
 
-        self.actor_optimizer = Adam(self.actor.parameters(), lr=lr_actor)
-        self.critic_optimizer = Adam(self.critic.parameters(), lr=lr_critic, weight_decay=weight_decay)
-        # self.actor_optimizer = AdamW(self.actor.parameters(), lr=lr_actor, betas=(0.9, 0.999), eps=1e-08, weight_decay=weight_decay, amsgrad=False)
-        # self.critic_optimizer = AdamW(self.critic.parameters(), lr=lr_critic, betas=(0.9, 0.999), eps=1e-08, weight_decay=weight_decay, amsgrad=False)
+        # self.actor_optimizer = Adam(self.actor.parameters(), lr=lr_actor)
+        # self.critic_optimizer = Adam(self.critic.parameters(), lr=lr_critic, weight_decay=weight_decay)
+        self.actor_optimizer = AdamW(self.actor.parameters(), lr=lr_actor, betas=(0.9, 0.999), eps=1e-08, weight_decay=weight_decay, amsgrad=False)
+        self.critic_optimizer = AdamW(self.critic.parameters(), lr=lr_critic, betas=(0.9, 0.999), eps=1e-08, weight_decay=weight_decay, amsgrad=False)
 
 
     def act(self, his, obs, noise=0.0):
