0a34043ea25243d2dc4f4bf264a5b29227cab37e
imasmitja <58853765+imasmitja@users.noreply.github.com>
2021-09-15 06:24:33 -0700

    update test 69

--- multiagent/scenarios/simple_track_ivan.py
+++ multiagent/scenarios/simple_track_ivan.py
@@ -115,7 +115,7 @@ class Scenario(BaseScenario):
                 world.error[i] = np.sqrt((l.pfxs[0]-world.landmarks[i].state.p_pos[0])**2+(l.pfxs[2]-world.landmarks[i].state.p_pos[1])**2) #Error from PF
             else:
                 world.error[i] = np.sqrt((l.lsxs[-1][0]-world.landmarks[i].state.p_pos[0])**2+(l.lsxs[-1][2]-world.landmarks[i].state.p_pos[1])**2) #Error from LS
-            # rew += 1.*(0.01-world.error[i])
+            rew += 1.*(0.01-world.error[i])
         
         dists = [np.sqrt(np.sum(np.square(agent.state.p_pos - l.state.p_pos))) for l in world.landmarks[:-world.num_landmarks]]
         
